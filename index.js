0eb67351441e0dcfea55dfa5346196bb75e6c0c6
shyyko.serhiy <shyyko.serhiy@gmail.com>
2018-09-21 00:33:32 -0400

    fix: increased scope permissions

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ app.get('/login', function (req, res) {
     res.cookie(stateKey, state);
 
     // your application requests authorization
-    const scope = 'user-read-private user-read-email';
+    const scope = 'user-modify-playback-state user-read-private user-read-email' +
+    ' user-read-playback-state playlist-read-private playlist-read-collaborative';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
